b024319bc2b0d80e1ff94a3ee62e6550e26f53ac
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-13 12:33:17 +0100

    Add missing copyright notice.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,23 @@
+#
+# Cookbook Name:: cloudfoundry
+# Recipe:: default
+#
+# Copyright 2012, Trotter Cashion
+# Copyright 2012, ZephirWorks
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # local_route is the IP address of a well known server on your network, it
 # is used to choose the right ip address (think of hosts that have multiple nics
 # and IP addresses assigned to them) of the host running the cloud controller. Default
