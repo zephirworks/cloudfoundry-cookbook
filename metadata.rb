ad96635831cfaf62b6e3df4d0a87adf810ce6413
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-13 14:38:35 +0100

    I am maintaining these cookbooks now.

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "Trotter Cashion"
-maintainer_email "cashion@gmail.com"
+maintainer       "Andrea Campi"
+maintainer_email "andrea.campi@zephirworks.com"
 license          "Apache 2.0"
 description      "Installs/Configures cloudfoundry"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
