1de130844696098372af05fb1f7980e5c6692d92
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-29 23:37:54 +0100

    Release 1.0.0: release early, release often.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "andrea.campi@zephirworks.com"
 license          "Apache 2.0"
 description      "Installs/Configures cloudfoundry"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.99.1"
+version          "1.0.0"
 
 %w{ ubuntu }.each do |os|
   supports os
