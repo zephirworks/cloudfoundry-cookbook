acfd0e99feecfdb43b79e573b3a92e112879d1fa
Andrea Campi <andrea.campi@zephirworks.com>
2013-01-06 23:23:13 +0100

    Release 1.2.0 for non-backward-compatible cleanup.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "andrea.campi@zephirworks.com"
 license          "Apache 2.0"
 description      "Installs/Configures cloudfoundry"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.2"
+version          "1.2.0"
 
 %w{ ubuntu }.each do |os|
   supports os
