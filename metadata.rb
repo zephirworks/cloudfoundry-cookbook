0006a455bdd35c81c3dca14f137bdf2838a093e1
Andrea Campi <andrea.campi@zephirworks.com>
2013-02-24 21:49:09 +0100

    Release 1.3.4: bump ruby version.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "andrea.campi@zephirworks.com"
 license          "Apache 2.0"
 description      "Installs/Configures cloudfoundry"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.3.3"
+version          "1.3.4"
 
 %w{ ubuntu }.each do |os|
   supports os
